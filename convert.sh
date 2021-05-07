dec2135620500050ae29b312f79f44c6af561f0a
signalwerk <sh@signalwerk.ch>
2021-05-07 19:21:24 +0200

    ADD: content

---
+++ convert.sh
@@ -0,0 +1,24 @@
+ffmpeg -i ./public/movies/course/js/basic/chapter/intro/overview/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/intro/overview/index.mp4
+
+ffmpeg -i ./public/movies/course/js/basic/chapter/intro/commments/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/intro/commments/index.mp4
+
+ffmpeg -i ./public/movies/course/js/basic/chapter/intro/function/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/intro/function/index.mp4
+
+ffmpeg -i ./public/movies/course/js/basic/chapter/variables/overview/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/variables/overview/index.mp4
+
+
+ffmpeg -i ./public/movies/course/js/basic/chapter/variables/assign/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/variables/assign/index.mp4
+
+
+ffmpeg -i ./public/movies/course/js/basic/chapter/variables/multiple/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/variables/multiple/index.mp4
+
+ffmpeg -i ./public/movies/course/js/basic/chapter/variables/use/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/variables/use/index.mp4
+
+
+
+
+ffmpeg -i ./public/movies/course/js/basic/chapter/types/overview/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/types/overview/index.mp4
+
+
+ffmpeg -i ./public/movies/course/js/basic/chapter/types/number/index.webm -c:v libx264 -crf 23 -filter:v "scale=-2:400, crop=400:400" -c:a aac -y ./public/movies/course/js/basic/chapter/types/number/index.mp4
+
